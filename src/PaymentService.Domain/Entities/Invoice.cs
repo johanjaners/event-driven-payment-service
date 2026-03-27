0677c0f2b547ae9768be2bdf42492f98a042686e
Johan Janérs <johanjaners@gmail.com>
2026-03-27 11:56:42 +0100

    Change TotalAmount type from int to decimal in Invoice class

--- src/PaymentService.Domain/Entities/Invoice.cs
+++ src/PaymentService.Domain/Entities/Invoice.cs
@@ -5,7 +5,7 @@ public class Invoice
     int Id;
     int BookingId;
     InvoiceStatus Status { get; set; } = InvoiceStatus.Pending;
-    int TotalAmount;
+    decimal TotalAmount;
     List<InvoiceLine> Lines;
     DateTime CreatedAt;
     DateTime UpdatedAt;
